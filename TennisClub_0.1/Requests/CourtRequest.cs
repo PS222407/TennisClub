2fe47823fae11f1b2be0f1a783edc394fc1a1416
Jens Ramakers <jensramakers@gmail.com>
2023-09-11 11:46:47 +0200

    validation

--- TennisClub_0.1/Requests/CourtRequest.cs
+++ TennisClub_0.1/Requests/CourtRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace TennisClub_0._1.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TennisClub_0._1.Requests;
 
 public class CourtRequest
 {
+    [Range(0, double.MaxValue, ErrorMessage = "Number must be a non-negative value.")]
     public int Number { get; set; }
 
     public bool Indoor { get; set; }
