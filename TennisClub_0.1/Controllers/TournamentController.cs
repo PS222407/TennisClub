a7b7db21a1dd897fa51154fca35e340f6cea87f6
Jens Ramakers <jensramakers@gmail.com>
2023-09-11 16:00:33 +0200

    added detail page for members

---
+++ TennisClub_0.1/Controllers/TournamentController.cs
@@ -0,0 +1,70 @@
+using BusinessLogicLayer.Interfaces;
+using DataLayer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using TennisClub_0._1.Models;
+
+namespace TennisClub_0._1.Controllers;
+
+public class TournamentController : Controller
+{
+    private readonly ITournamentService _tournamentService;
+    
+    public TournamentController(ITournamentService tournamentService)
+    {
+        _tournamentService = tournamentService;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        List<TournamentDto>? tournamentDtos = _tournamentService.GetAll();
+        if (tournamentDtos == null)
+        {
+            TempData["Message"] = "Fout tijdens het ophalen van data.";
+            TempData["MessageType"] = "danger";
+
+            return View(new List<TournamentViewModel>());
+        }
+
+        List<TournamentViewModel> tournamentViewModels = new List<TournamentViewModel>();
+        foreach (TournamentDto tournamentDto in tournamentDtos)
+        {
+            tournamentViewModels.Add(new TournamentViewModel
+            {
+                Id = tournamentDto.Id,
+                Name = tournamentDto.Name,
+                Description = tournamentDto.Description,
+                Price = tournamentDto.Price,
+                MaxMembers = tournamentDto.MaxMembers,
+                StartDateTime = tournamentDto.StartDateTime,
+            });
+        }
+
+        return View(tournamentViewModels);
+    }
+    
+    // GET: Tournaments/Details/5
+    public ActionResult Details(int id)
+    {
+        TournamentDto? tournamentDto = _tournamentService.FindById(id);
+        if (tournamentDto == null)
+        {
+            TempData["Message"] = "Fout tijdens het ophalen van data.";
+            TempData["MessageType"] = "danger";
+
+            return View();
+        }
+
+        TournamentViewModel tournamentViewModel = new TournamentViewModel
+        {
+            Id = tournamentDto.Id,
+            Name = tournamentDto.Name,
+            Description = tournamentDto.Description,
+            Price = tournamentDto.Price,
+            MaxMembers = tournamentDto.MaxMembers,
+            StartDateTime = tournamentDto.StartDateTime,
+        };
+
+        return View(tournamentViewModel);
+    }
+}
